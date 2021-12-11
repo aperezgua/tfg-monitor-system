b5d0aa72bf213e1f5a36da56eb11f3f0c6720225
aperezgua <aperezgua@uoc.edu>
2021-12-11 14:14:28 +0100

    Update LogControllerTest.java

--- log-microservice/src/test/java/edu/uoc/tfgmonitorsystem/logmicroservice/controller/LogControllerTest.java
+++ log-microservice/src/test/java/edu/uoc/tfgmonitorsystem/logmicroservice/controller/LogControllerTest.java
@@ -2,10 +2,13 @@ package edu.uoc.tfgmonitorsystem.logmicroservice.controller;
 
 import edu.uoc.tfgmonitorsystem.common.controller.security.JwtConstants;
 import edu.uoc.tfgmonitorsystem.common.controller.security.JwtTokenUtil;
+import edu.uoc.tfgmonitorsystem.common.model.document.Agent;
 import edu.uoc.tfgmonitorsystem.common.model.document.Log;
 import edu.uoc.tfgmonitorsystem.common.model.document.Rol;
 import edu.uoc.tfgmonitorsystem.common.model.document.User;
+import edu.uoc.tfgmonitorsystem.common.model.exception.TfgMonitorSystenException;
 import edu.uoc.tfgmonitorsystem.logmicroservice.model.dto.AgentLogFilter;
+import edu.uoc.tfgmonitorsystem.logmicroservice.model.service.ILogService;
 import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
@@ -21,21 +24,35 @@ import org.springframework.http.ResponseEntity;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class LogControllerTest {
 
+    private static final String AGENT_TOKEN_ID = "0bac5204-4951-11ec-81d3-0242ac130003";
+
     @Autowired
     private TestRestTemplate testRestTemplate;
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
+    @Autowired
+    private ILogService logService;
+
+    public void createLog(String logLine) throws TfgMonitorSystenException {
+        Log log = new Log();
+        log.setAgent(new Agent(AGENT_TOKEN_ID));
+        log.setLogLine(logLine);
+        logService.createLog(log);
+    }
+
     /**
      * Se prueba la petición de búsqueda por regexp.
+     *
+     * @throws TfgMonitorSystenException
      */
     @SuppressWarnings("unchecked")
     @Test
-    public void findByRegexpTest() {
+    public void findByRegexpTest() throws TfgMonitorSystenException {
 
         AgentLogFilter regexpFilter = new AgentLogFilter();
-        regexpFilter.setAgentTokenId("0bac5204-4951-11ec-81d3-0242ac130003");
+        regexpFilter.setAgentTokenId(AGENT_TOKEN_ID);
 
         ResponseEntity<Object> response = testRestTemplate.postForEntity("/rest/log/findByRegexp", regexpFilter,
                 Object.class);
@@ -45,6 +62,47 @@ public class LogControllerTest {
         List<Log> logs = (List<Log>) response.getBody();
 
         Assertions.assertEquals(Integer.valueOf(0), Integer.valueOf(logs.size()));
+
+        createLog("Lalalla");
+
+        response = testRestTemplate.postForEntity("/rest/log/findByRegexp", regexpFilter, Object.class);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        logs = (List<Log>) response.getBody();
+
+        Assertions.assertEquals(Integer.valueOf(1), Integer.valueOf(logs.size()));
+
+        logService.pruneLog(0L);
+
+    }
+
+    @Test
+    public void putLogTest() throws TfgMonitorSystenException {
+        Agent agent = new Agent(AGENT_TOKEN_ID);
+        testRestTemplate.getRestTemplate().setInterceptors(Collections.singletonList((request, body, execution) -> {
+            request.getHeaders().add(JwtConstants.AUTHORIZATION_HEADER, jwtTokenUtil.generateToken(agent));
+            return execution.execute(request, body);
+        }));
+
+        String output = testRestTemplate.postForObject("/rest/log/put", "Nueva linea de log", String.class);
+
+        Assertions.assertEquals("OK", output);
+
+        logService.pruneLog(0L);
+    }
+
+    @Test
+    public void updateAgentEvents() {
+        AgentLogFilter filter = new AgentLogFilter();
+        filter.setAgentTokenId(AGENT_TOKEN_ID);
+
+        ResponseEntity<String> response = testRestTemplate.postForEntity("/rest/log/updateAgentEvents", filter,
+                String.class);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        Assertions.assertEquals("OK", response.getBody());
     }
 
     /**
